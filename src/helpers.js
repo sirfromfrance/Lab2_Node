e1688fbc28fe0de826e05ec2ba703a9eb6a361b6
Valeriia Hrytsenko <gritsenko5555@gmail.com>
2024-01-20 22:59:58 +0200

    add comments

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,10 @@
-function json(data)
+/**
+ * A util method for safety parse your data to JSON :]
+ * @param {any} data - your data which you wanna safety parse into JSON
+ * @param {any} fallback - for catch purpose
+ * @returns
+ */
+function json(data) 
 {
     this.end(JSON.stringify(data))
 }

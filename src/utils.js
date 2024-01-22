5f6c4c762cb3cd7b6e0acfaf8a315e7e54929594
Valeriia Hrytsenko <gritsenko5555@gmail.com>
2024-01-22 20:52:44 +0200

    add comments

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,9 @@
+/**
+ * A util method for safety parse your data to JSON :]
+ * @param {any} data - your data which you wanna safety parse into JSON
+ * @param {any} fallback - for catch purpose
+ * @returns
+ */
 function safeJSON(data, fallback)
 {
     try{
